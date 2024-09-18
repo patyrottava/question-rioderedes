e01750ce612079ceee9d8f8f52d3d52374e8b464
patyrotava <patriciarottavatriches@gmail.com>
2024-09-18 14:59:41 -0300

    first commit

---
+++ questionario.py
@@ -0,0 +1,51 @@
+print('''
+       
+        Seja bem-vindo ao jogo de perguntas e respostas.
+       
+       
+       você responderá 5 perguntas, e saberá se acertou ou não
+
+       ''')
+
+print ('Vamos começar!')
+
+
+print ('Qual das linguagens estamos estudando')
+print( 'a)Java')
+print( 'b)Python')
+print( '''c)CSS
+      ''')
+
+
+resposta = input('digite uma das opções')
+
+
+
+if resposta =='b':  
+    print('Acertou')
+if resposta == 'c':
+    print('você errou')
+if resposta == 'a':
+    print('você errou')
+
+
+    print ('O que é um commit?')
+print( 'a)salvar alterações no repositório local')
+print( 'b)salvar alterações no git hub')
+print( 'c)realizar um push')
+
+resposta = input('digite uma das opções')
+
+
+
+if resposta =='b':
+    print('Errou')
+if resposta == 'c':
+    print('você errou')
+if resposta == 'a':
+    print('você acertou')
+
+
+
+
+
